17103ae6999c53df72f8488253ce61ed96b490f7
DannaValentinaR <dvramirez078@misena.edu.co>
2024-07-21 23:25:42 -0500

    config:Login

---
+++ app/src/main/java/co/edu/sena/login2902081d/MainActivity.kt
@@ -0,0 +1,26 @@
+package co.edu.sena.login2902081d
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import co.edu.sena.login2902081d.app.PostOfficeApp
+import co.edu.sena.login2902081d.screeans.SignUpScreen
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            SignUpScreen()
+        }
+    }
+}
+
+
+
+
